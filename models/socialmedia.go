56e38504b715e0539712440ca6d74d2bd4afbc63
rifqoi <alfurqon.rifqi@gmail.com>
2022-11-02 16:40:09 +0700

    feat: add social media models and change gorm tag in user struct

---
+++ models/socialmedia.go
@@ -0,0 +1,16 @@
+package models
+
+import "time"
+
+type SocialMedia struct {
+	ID             int    `json:"id" gorm:"primaryKey"`
+	Name           string `json:"name" gorm:"notNull"`
+	SocialMediaUrl string `json:"social_media_url" gorm:"notNull"`
+	UserID         int    `json:"user_id" gorm:"notNull"`
+
+	// User disini untuk mereferensi UserID
+	// https://stackoverflow.com/questions/73661393/has-one-vs-belongs-to-in-gorm
+	User      User      `json:"-"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
--- models/user.go
+++ models/user.go
@@ -5,11 +5,11 @@ import (
 )
 
 type User struct {
-	ID        int       `json:"id" gorm:"primaryKey"`
-	Email     string    `json:"email" gorm:"uniqueIndex"`
-	Username  string    `json:"username" gorm:"uniqueIndex"`
-	Password  string    `json:"password"`
-	Age       int       `json:"age"`
+	ID        int       `json:"id" gorm:"primaryKey;notNull"`
+	Email     string    `json:"email" gorm:"uniqueIndex;notNull"`
+	Username  string    `json:"username" gorm:"uniqueIndex;notNull"`
+	Password  string    `json:"password" gorm:"notNull"`
+	Age       int       `json:"age" gorm:"notNull"`
 	CreatedAt time.Time `json:"-"`
 	UpdatedAt time.Time `json:"-"`
 }
