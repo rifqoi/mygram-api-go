9e2b239a8b15ec3b27f4d0759a13212f1e989657
rifqoi <alfurqon.rifqi@gmail.com>
2022-11-07 14:16:26 +0700

    refactor: jwt validation and generate jwt token

--- api/middlewares/auth.go
+++ api/middlewares/auth.go
@@ -4,7 +4,6 @@ import (
 	"strings"
 
 	"github.com/gin-gonic/gin"
-	"github.com/golang-jwt/jwt"
 	"github.com/jusidama18/mygram-api-go/api/responses"
 	"github.com/jusidama18/mygram-api-go/helpers"
 	"github.com/jusidama18/mygram-api-go/services"
@@ -20,43 +19,32 @@ func NewMiddleware(userService *services.UserService) *Middleware {
 	}
 }
 
-func (m *Middleware) Authorization() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		authHeader := c.GetHeader("Authorization")
+func (m *Middleware) Authorization(c *gin.Context) {
+	authHeader := c.GetHeader("Authorization")
 
-		if !strings.HasPrefix(authHeader, "Bearer ") {
-			responses.UnauthorizedRequest(c, "JWT Token must be provided.")
-			return
-		}
-		tokenString := strings.Replace(authHeader, "Bearer ", "", -1)
-
-		token, err := helpers.ValidateToken(tokenString)
-		if err != nil {
-			responses.UnauthorizedRequest(c, err.Error())
-			return
-		}
-		claims, ok := token.Claims.(jwt.MapClaims)
-		if !ok || !token.Valid {
-			responses.UnauthorizedRequest(c, "Invalid token.")
-			return
-		}
-
-		// Cek apakah "id" exist didalam map claims
-		if _, exist := claims["id"]; !exist {
-			responses.UnauthorizedRequest(c, "Invalid token")
-			return
-		}
-
-		// Default unmarshal dari encoding/json itu ke float64 sehingga di cast ke float64
-		// https://stackoverflow.com/questions/70705673/panic-interface-conversion-interface-is-float64-not-int64
-		id := int(claims["id"].(float64))
-		user, err := m.userService.FindUserByID(id)
-		if err != nil {
-			responses.UnauthorizedRequest(c, err.Error())
-			return
-		}
-
-		c.Set("claims", claims)
-		c.Set("userInfo", user)
+	if !strings.HasPrefix(authHeader, "Bearer ") {
+		responses.UnauthorizedRequest(c, "JWT Token must be provided.")
+		return
 	}
+	tokenString := strings.Replace(authHeader, "Bearer ", "", -1)
+
+	claims, err := helpers.ValidateToken(tokenString)
+	// Cek apakah "id" exist didalam map claims
+	if _, exist := claims["id"]; !exist {
+		responses.UnauthorizedRequest(c, "Id not exists!")
+		return
+	}
+
+	// Default unmarshal dari encoding/json itu ke float64 sehingga di cast ke float64
+	// https://stackoverflow.com/questions/70705673/panic-interface-conversion-interface-is-float64-not-int64
+	id := int(claims["id"].(float64))
+	user, err := m.userService.FindUserByID(id)
+	if err != nil {
+		responses.UnauthorizedRequest(c, err.Error())
+		return
+	}
+
+	c.Set("claims", claims)
+	c.Set("userInfo", user)
+	c.Next()
 }
--- helpers/jwt.go
+++ helpers/jwt.go
@@ -6,21 +6,30 @@ import (
 
 	"github.com/golang-jwt/jwt"
 	"github.com/jusidama18/mygram-api-go/config"
-	"github.com/jusidama18/mygram-api-go/models"
 )
 
 var JWT_SIGNING_METHOD = jwt.SigningMethodHS256
 var JWT_SIGNING_KEY = []byte(config.GetEnv("JWT_SIGNING_KEY"))
 
-func GenerateToken(user *models.User) (string, error) {
+type JWTClaims struct {
+	jwt.StandardClaims
+	Email string `json:"email"`
+	Id    int    `json:"id"`
+}
+
+func GenerateToken(email string, id int) (string, error) {
 	LOGIN_EXPIRATION_DURATION := time.Now().Add(time.Hour * 24 * 7).Unix()
 
-	claim := jwt.MapClaims{}
-	claim["email"] = user.Email
-	claim["id"] = user.ID
-	claim["exp"] = LOGIN_EXPIRATION_DURATION
-	claim["iat"] = time.Now().Unix()
-	token := jwt.NewWithClaims(JWT_SIGNING_METHOD, claim)
+	claims := JWTClaims{
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: LOGIN_EXPIRATION_DURATION,
+			IssuedAt:  time.Now().Unix(),
+		},
+		Email: email,
+		Id:    id,
+	}
+
+	token := jwt.NewWithClaims(JWT_SIGNING_METHOD, claims)
 
 	// SignedString harus type []byte
 	signedToken, err := token.SignedString(JWT_SIGNING_KEY)
@@ -28,7 +37,8 @@ func GenerateToken(user *models.User) (string, error) {
 	return signedToken, err
 }
 
-func ValidateToken(encToken string) (*jwt.Token, error) {
+func ValidateToken(encToken string) (jwt.MapClaims, error) {
+	// Cek signing method dari token
 	token, err := jwt.Parse(encToken, func(t *jwt.Token) (interface{}, error) {
 		if method, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, fmt.Errorf("Invalid signing method.")
@@ -37,5 +47,23 @@ func ValidateToken(encToken string) (*jwt.Token, error) {
 		}
 		return JWT_SIGNING_KEY, nil
 	})
-	return token, err
+	if err != nil {
+		return nil, err
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok || !token.Valid {
+		return nil, fmt.Errorf("Invalid token")
+	}
+
+	// Convert dari claims ke int64 agar bisa diubah ke tipe data time.Time
+	exp := int64(claims["exp"].(float64))
+
+	// Convert unix timestamp ke tipe data time.Time
+	expiredAt := time.Unix(exp, 0)
+	if time.Now().After(expiredAt) {
+		return nil, fmt.Errorf("Token expired.")
+	}
+
+	return claims, err
 }
